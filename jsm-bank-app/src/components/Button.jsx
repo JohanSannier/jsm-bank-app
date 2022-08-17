361dc4d884b1a36d7bbf95b61d7ffa1795ee9fa7
JohanSannier <johan.sanniercaffier@gmail.com>
2022-08-17 11:02:41 +0200

    CardDeal

--- jsm-bank-app/src/components/Button.jsx
+++ jsm-bank-app/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-full transition ease-in-out hover:-translate-y-1 hover:scale-110`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] transition ease-in-out hover:-translate-y-1 hover:scale-110`}
     >
       Get Started
     </button>
--- jsm-bank-app/src/components/CardDeal.jsx
+++ jsm-bank-app/src/components/CardDeal.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={`${layout.section}`}>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={`${styles.heading2}`}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        No need to spend hours looking through the web to get the best deals, we
+        gather the best deals for you directly in the app.
+      </p>
+      <Button styles="mt-10" />
+    </div>
 
+    <div className={`${layout.sectionImg}`}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 export default CardDeal;
